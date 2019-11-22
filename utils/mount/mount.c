openunix/nfs-utils

commit d5e303461846a5c7282b819fb729760bef472d89

mount: Do not overwrite /etc/mtab if it's symlink
Some systems have /etc/mtab symlink to /proc/mounts. In that case
mount.nfs complains:
Can't set permissions on mtab: Operation not permitted

See https://bugs.debian.org/476577

This change makes mount.nfs handle symlinked /etc/mtab the way
umount.nfs and util- linux handle it.

Cc: Chuck Lever <chuck.lever@oracle.com>
Signed-off-by: Joey Hess <joeyh@debian.org>
[ pvorel: took patch from Debian, rebased for 2.4.3-rc1 and created commit
message. Patch is also used in Gentoo. ]
Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
Signed-off-by: Steve Dickson <steved@redhat.com>

utils/mount/mount.c
@@ -204,6 +204,13 @@ create_mtab (void) {
 	int flags;
 	mntFILE *mfp;
 
+	/* Avoid writing if the mtab is a symlink to /proc/mounts, since
+	   that would create a file /proc/mounts in case the proc filesystem
+	   is not mounted, and the fchmod below would also fail. */
+	if (mtab_is_a_symlink()) {
+		return EX_SUCCESS;
+	}
+
 	lock_mtab();
 
 	mfp = nfs_setmntent (MOUNTED, "a+");

utils/mount/fstab.c
@@ -61,7 +61,7 @@ mtab_does_not_exist(void) {
 	return var_mtab_does_not_exist;
 }
 
-static int
+int
 mtab_is_a_symlink(void) {
         get_mtab_info();
         return var_mtab_is_a_symlink;

utils/mount/fstab.h
@@ -7,6 +7,7 @@
 #define _PATH_FSTAB "/etc/fstab"
 #endif
 
+int mtab_is_a_symlink(void);
 int mtab_is_writable(void);
 int mtab_does_not_exist(void);
 void reset_mtab_info(void);

commit f57d955c9ce6e208ac40fc78d6b23c8bd09c3755

mount: Fix return 0 from void function
Fixes: d5e30346 ("mount: Do not overwrite /etc/mtab if it's symlink")

Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
Signed-off-by: Steve Dickson <steved@redhat.com>

utils/mount/mount.c
@@ -208,7 +208,7 @@ create_mtab (void) {
 	   that would create a file /proc/mounts in case the proc filesystem
 	   is not mounted, and the fchmod below would also fail. */
 	if (mtab_is_a_symlink()) {
-		return EX_SUCCESS;
+		return;
 	}
 
 	lock_mtab();